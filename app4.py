3567e3e634b455ff38406b838ea29001213e53a1
Josh Fernandes <joshfernandes@mac.com>
2012-10-29 00:15:51 -0700

    trying to use blueprint

--- app4.py
+++ app4.py
@@ -3,12 +3,19 @@ import json
 from flask import Flask
 
 app = Flask(__name__)
+from flask import Blueprint, render_template, abort
+from jinja2 import TemplateNotFound
 
-@app.route('/physics/standardgravity')
-def hello():
-    dict = {'value': 9.80665, 'units': 'meters/(seconds^2)', 'citation': 'http://en.wikipedia.org/wiki/Standard_gravity','name': 'standard gravity'};
-    data = json.dumps(dict)
-    return data
+simple_page = Blueprint('simple_page', __name__,
+                        template_folder='templates')
+
+@simple_page.route('/', defaults={'page': 'index'})
+@simple_page.route('/<page>')
+def show(page):
+    try:
+        return render_template('pages/%s.html' % page)
+    except TemplateNotFound:
+        abort(404)
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
