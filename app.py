ef55388559f25e3290c7b490818200c9adce1d6f
Josh Fernandes <joshfernandes@mac.com>
2012-10-29 17:05:27 -0700

    whoo

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import flask, flask.views
 
 app = flask.Flask(__name__)
@@ -7,3 +8,8 @@ class View(flask.views.MethodView):
 		return "Hello World!"
 
 app.add_url_rule('/', view_func=View.as_view('main'))
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
