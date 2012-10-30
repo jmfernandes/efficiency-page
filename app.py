01c724a356f3c5405a348f7ba92cabeb7de4bb84
Josh Fernandes <joshfernandes@mac.com>
2012-10-29 19:00:29 -0700

    test

--- app.py
+++ app.py
@@ -3,14 +3,16 @@ import flask, flask.views
 
 app = flask.Flask(__name__)
 
+app.secret_key = "cheese"
+
 class View(flask.views.MethodView):
 	def get(self):
 		return flask.render_template('efficiency.html')
+            
+def post(self):
+    return str(flask.request.form['expression'])
 
-    def post(self):
-        return str(flask.request.form['expression'])
-
-app.add_url_rule('/', view_func=View.as_view('main'), methods=['get','post'])
+app.add_url_rule('/', view_func=View.as_view('main'), methods=['GET','POST'])
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
