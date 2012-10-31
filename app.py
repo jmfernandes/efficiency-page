2486340ef247f049695c9789c2218520a08dca79
Josh Fernandes <joshfernandes@mac.com>
2012-10-31 15:57:44 -0700

    new app

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
-import flask, flask.views
-import os
-app = flask.Flask(__name__)
-# Don't do this!
-app.secret_key = "bacon"
+from werkzeug.routing import Map, Rule, NotFound, RequestRedirect
 
-class View(flask.views.MethodView):
-    def get(self):
-        return flask.render_template('index.html')
-    def post(self):
-        #result = eval(flask.request.form['expression'])
-        #flask.flash(result)
-        return 'hello'
+url_map = Map([
+    Rule('/', endpoint='blog/index'),
+    Rule('/<int:year>/', endpoint='blog/archive'),
+    Rule('/<int:year>/<int:month>/', endpoint='blog/archive'),
+    Rule('/<int:year>/<int:month>/<int:day>/', endpoint='blog/archive'),
+    Rule('/<int:year>/<int:month>/<int:day>/<slug>',
+         endpoint='blog/show_post'),
+    Rule('/about', endpoint='blog/about_me'),
+    Rule('/feeds/', endpoint='blog/feeds'),
+    Rule('/feeds/<feed_name>.rss', endpoint='blog/show_feed')
+])
 
-app.add_url_rule('/', view_func=View.as_view('main'), methods=['GET', 'POST'])
+def application(environ, start_response):
+    urls = url_map.bind_to_environ(environ)
+    try:
+        endpoint, args = urls.match()
+    except HTTPException, e:
+        return e(environ, start_response)
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return ['Rule points to %r with arguments %r' % (endpoint, args)]
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
