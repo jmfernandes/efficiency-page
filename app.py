7abd1aa5d30cd4caf9caff6ff3fba3ecaf100d7a
Josh Fernandes <joshfernandes@mac.com>
2012-11-27 21:20:52 -0800

    fixed footer

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from flask import render_template
 
 app = Flask(__name__)
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
+
 @app.route('/')
 def index():
     return  render_template('efficiency.html')
