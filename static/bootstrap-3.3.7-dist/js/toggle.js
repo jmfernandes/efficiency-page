e732d37a6bd50b2fc788c46a16ca2019c28c47e0
Josh Fernandes <joshfernandes@mac.com>
2017-01-16 16:11:01 -0800

    fixes

---
+++ static/bootstrap-3.3.7-dist/js/toggle.js
@@ -0,0 +1,13 @@
+function toggle(id){
+    ul = "ul_" + id;
+    img = "img_" + id;
+    ulElement = document.getElementById(ul);
+    imgElement = document.getElementById(img);
+    if (ulElement){
+            if (ulElement.className == 'closed'){
+                    ulElement.className = "open";
+                    }else{
+                    ulElement.className = "closed";
+                    }
+            }
+    }
